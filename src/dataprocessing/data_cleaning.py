b1986afdca07a5c866721ed9b1efe406416140ce
nguyen-brat <julius.2003.no.2@gmail.com>
2024-07-25 02:31:12 +0700

    add some general file structure

---
+++ src/dataprocessing/data_cleaning.py
@@ -0,0 +1,32 @@
+from glob import glob
+import os
+import sys
+import tqdm
+
+from .extract_text import PdfOCR
+import pdf2image
+import pdfplumber
+
+class PdfParsing:
+    '''
+    Parser all document include scan pdf, pdf, docx to text
+    '''
+    def __init__(self):
+        pass
+
+    def __call__(self, doc_abstract_path):
+        pass
+
+    def transform(self, input_path, output_path):
+        pass
+
+
+class DocGraph:
+    '''
+    Find the name of document and other document mentioned
+    '''
+    def __init__(self):
+        pass
+
+if __name__ == "__main__":
+    pass
